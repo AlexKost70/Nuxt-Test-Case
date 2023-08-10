39e8d27ed642acdfc4fa53ca1591334e7580dc54
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-08-10 18:39:09 +0300

    Updates

--- _nuxt/static/1691681182/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNuxt-Test-Case\u002F_nuxt\u002Fstatic\u002F1691681182",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNuxt-Test-Case\u002F",assetsPath:"\u002FNuxt-Test-Case\u002F_nuxt\u002F",cdnURL:a}}}}(null));
